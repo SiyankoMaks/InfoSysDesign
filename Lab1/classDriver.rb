69114dfc160255eda94d689d9dc70061d1db38af
Maks <sianko0608@gmail.com>
2024-09-27 14:44:33 +0300

    Deleted duplicates and corrected validations

--- Lab1/classDriver.rb
+++ Lab1/classDriver.rb
@@ -4,7 +4,7 @@ class Driver
 
   # Конструктор для инициализации объекта Driver
   def initialize(id, last_name, first_name, middle_name, experience)
-    @id = id
+    @id = validate_id(id) # валидация id
     self.last_name = last_name
     self.first_name = first_name
     self.middle_name = middle_name
@@ -13,51 +13,40 @@ class Driver
 
   # Статический метод для создания объекта Driver с проверкой всех полей
   def self.create_driver(id, last_name, first_name, middle_name, experience)
-    raise 'ID должен быть положительным числом' unless id.is_a?(Integer) && id > 0
-    validate_last_name(last_name)
-    validate_first_name(first_name)
-    validate_middle_name(middle_name)
-    validate_experience(experience)
-    
     new(id, last_name, first_name, middle_name, experience)
   end
 
   # Валидации для полей
-  def self.validate_last_name(last_name)
-    raise 'Фамилия не может быть пустой' if last_name.nil? || last_name.strip.empty?
+  def validate_id(id)
+    raise 'ID должен быть положительным числом' unless id.is_a?(Integer) && id > 0
+    id
   end
 
-  def self.validate_first_name(first_name)
-    raise 'Имя не может быть пустым' if first_name.nil? || first_name.strip.empty?
+  def validate_name(name, field_name)
+    raise "#{field_name} не может быть пустым" if name.nil? || name.strip.empty?
+    name
   end
 
-  def self.validate_middle_name(middle_name)
-    raise 'Отчество не может быть пустым' if middle_name.nil? || middle_name.strip.empty?
-  end
-
-  def self.validate_experience(experience)
+  def validate_experience(experience)
     raise 'Стаж должен быть положительным числом' unless experience.is_a?(Integer) && experience >= 0
+    experience
   end
 
   # Сеттеры с валидацией
   def last_name=(last_name)
-    self.class.validate_last_name(last_name)
-    @last_name = last_name
+    @last_name = validate_name(last_name, "Фамилия")
   end
 
   def first_name=(first_name)
-    self.class.validate_first_name(first_name)
-    @first_name = first_name
+    @first_name = validate_name(first_name, "Имя")
   end
 
   def middle_name=(middle_name)
-    self.class.validate_middle_name(middle_name)
-    @middle_name = middle_name
+    @middle_name = validate_name(middle_name, "Отчество")
   end
 
   def experience=(experience)
-    self.class.validate_experience(experience)
-    @experience = experience
+    @experience = validate_experience(experience)
   end
 
   # Метод Информация о водителе
